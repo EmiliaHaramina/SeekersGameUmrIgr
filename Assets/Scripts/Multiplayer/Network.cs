44770ea94684bbedc7f66096f9bd9ed25b3a6938
Mateo Paladin <mp52512@fer.hr>
2024-01-20 10:32:12 +0100

    Created new spawn points

--- Assets/Scripts/Multiplayer/Network.cs
+++ Assets/Scripts/Multiplayer/Network.cs
@@ -8,16 +8,36 @@ public class Network : MonoBehaviour
     public MasterClient masterClient;
 
     private GameObject spawnedPlayerPrefab;
+    private int nextSpawnIndex = 0;
 
-    [SerializeField] private GameObject hiderSpawnPoint;
+    [SerializeField] private GameObject[] hiderSpawnPoint;
+
 
     private void Start()
     {
-        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", hiderSpawnPoint.transform.position, hiderSpawnPoint.transform.rotation);
+
+        PhotonNetwork.Instantiate("NetworkPlayerPrefab", GetNextSpawnPoint(), Quaternion.identity);
+   
+
+
         if (PhotonNetwork.IsMasterClient)
         {
             masterClient.Initialize();
         }
+    }
+
+    Vector3 GetNextSpawnPoint()
+    {
+        if (hiderSpawnPoint.Length == 0)
+        {
+            Debug.LogError("No spawn points assigned.");
+            return Vector3.zero;
+        }
+
+        Vector3 spawnPoint = hiderSpawnPoint[nextSpawnIndex].transform.position;
+        nextSpawnIndex = (nextSpawnIndex + 1) % hiderSpawnPoint.Length; // Move to the next spawn point
+
+        return spawnPoint;
     }
 
     //public override void OnJoinedRoom()
